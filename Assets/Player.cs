246c542f113c4af0c32795ea37097190d7eba443
The210 <enano.endemoniado@gmail.com>
2020-07-24 02:44:27 +0200

    Add rope resize

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Security.Cryptography;
 using System.Security.Permissions;
 using UnityEngine;
 
@@ -9,11 +10,15 @@ public class Player : MonoBehaviour
     public Camera mainCamera;
     public LineRenderer _lineRenderer;
     public DistanceJoint2D _distanceJoint;
+    public GameObject grapplePoints;
 
     public float rocketStrength;
     public float reelStrength;
+    public float maxGrappleDistance;
+    public float maxMouseGrapple;
 
     private Rigidbody2D rb;
+    private DrawRope drawRope;
     private Vector3 prevPos;
     private Vector3 movementVector;
     
@@ -22,6 +27,8 @@ public class Player : MonoBehaviour
     {
         _distanceJoint.enabled = false;
         rb = GetComponent<Rigidbody2D>();
+        drawRope = GetComponent<DrawRope>();
+        _lineRenderer.enabled = false;
     }
 
     // Update is called once per frame
@@ -30,15 +37,30 @@ public class Player : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Transform grappled = null;
             Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            _lineRenderer.SetPosition(0, mousePos);
-            _lineRenderer.SetPosition(1, transform.position);
-            _distanceJoint.connectedAnchor = mousePos;
-            _distanceJoint.enabled = true;
-            _lineRenderer.enabled = true;
+            float closestMouseGrapple = maxMouseGrapple;
+            foreach (Transform grapplePoint in grapplePoints.transform) 
+            {
+                if (Vector3.Distance(mousePos, grapplePoint.position) < closestMouseGrapple && Vector3.Distance(this.transform.position, grapplePoint.position) < maxGrappleDistance) 
+                {
+                    closestMouseGrapple = Vector3.Distance(mousePos, grapplePoint.position);
+                    grappled = grapplePoint;
+                }
+            }
+            if (closestMouseGrapple < maxMouseGrapple) 
+            {
+                drawRope.segmentLength = (int) (Vector3.Distance(this.transform.position, grappled.position) / (drawRope.ropeSegLen * 2));
+                drawRope.reInitializeRope();
+                drawRope.EndPoint = grappled;
+                _distanceJoint.connectedAnchor = grappled.position;
+                _distanceJoint.enabled = true;
+                _lineRenderer.enabled = true;
+            }
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
+            drawRope.EndPoint = this.transform;
             _distanceJoint.enabled = false;
             _lineRenderer.enabled = false;
         }
@@ -60,6 +82,12 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(Vector3.Normalize(movementVector) * rocketStrength * -1);
         }
-        _distanceJoint.distance -= Input.mouseScrollDelta.y * reelStrength;
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            _distanceJoint.distance -= Input.mouseScrollDelta.y * reelStrength;
+            int segmentLength = (int)(_distanceJoint.distance / drawRope.ropeSegLen);
+            drawRope.resizeRope(segmentLength);
+        }
+
     }
 }
