b046e405b81787e385f75a14986a3596d1025e39
The210 <enano.endemoniado@gmail.com>
2020-07-31 22:16:09 +0200

    Implement black and white hole

---
+++ Assets/GravityPuller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPuller : MonoBehaviour
+{
+    public float strength;
+    public float range;
+
+    private GameObject player;
+    private Rigidbody2D playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerRb = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < range)
+        {
+            Pull();
+        }
+    }
+
+    private void Pull()
+    {
+        float pushForce = Math.Abs((range - Vector3.Distance(transform.position, player.transform.position)) * strength) * -1;
+        Vector3 pushDirection = (player.transform.position - transform.position).normalized;
+        playerRb.AddForce(pushDirection * pushForce);
+    }
+}
---
+++ Assets/GravityPusher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPusher : MonoBehaviour
+{
+    public float strength;
+    public float range;
+
+    private GameObject player;
+    private Rigidbody2D playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerRb = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < range)
+        {
+            Push();
+        }
+    }
+
+    private void Push()
+    {
+        float pushForce = Math.Abs((range - Vector3.Distance(transform.position, player.transform.position)) * strength);
+        Vector3 pushDirection = (player.transform.position - transform.position).normalized;
+        playerRb.AddForce(pushDirection * pushForce);
+    }
+}
