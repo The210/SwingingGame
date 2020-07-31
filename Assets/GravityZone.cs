c9f3cfa38a6398a0bce2f9a83217ddd52ab4e5ea
The210 <enano.endemoniado@gmail.com>
2020-08-01 00:19:19 +0200

    Add gravity zone functionality, work on PS

---
+++ Assets/GravityZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityZone : MonoBehaviour
+{
+    public float gravityScaleChange;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("enters collider");
+        if (collision.tag == "Player")
+        {
+            print("enters collider as player");
+            collision.gameObject.GetComponent<Rigidbody2D>().gravityScale += gravityScaleChange;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().gravityScale -= gravityScaleChange;
+        }
+    }
+}
